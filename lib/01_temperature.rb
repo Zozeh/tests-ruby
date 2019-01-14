47a047f2c02f66d92ff9f91bd2b22968f14528a9
zozeh <joseonetouch8@gmail.com>
2019-01-14 20:38:29 +0100

    version finale

---
+++ lib/01_temperature.rb
@@ -0,0 +1,12 @@
+def ftoc(degree)
+  degree = degree
+  celcius = ((degree - 32)/1.8)
+  return celcius  
+end 
+
+
+def ctof(degre)
+  degre = degre
+  fahrenheit = ((degre * 1.8)+ 32)
+  return fahrenheit
+end
